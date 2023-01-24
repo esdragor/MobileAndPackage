e3cf5a9f6b702a2c02e9c64ad7e84c888458e27a
esdragor <64788862+esdragor@users.noreply.github.com>
2023-01-24 17:03:19 +0100

    more helper

--- MobileAndPackage/Assets/Source/Helpers.cs
+++ MobileAndPackage/Assets/Source/Helpers.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using Codice.Client.Commands;
 using UnityEngine;
 using UnityEngine.UIElements;
 using static UnityEngine.Color;
+using Object = UnityEngine.Object;
 
 public class Helpers
 {
@@ -60,5 +63,51 @@ public class Helpers
         if (d == blue)
             return "<color=blue>" + msg + "</color>";
         return "<color=white>" + msg + "</color>";
+    }
+
+    public class Singleton<T> : MonoBehaviour where T : class
+    {
+        public static T Instance;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public class Pool<T> where T : Object
+    {
+        public readonly Queue<T> pool = new Queue<T>();
+        public readonly T defaultItem;
+        
+        public Pool(T defaultItem)
+        {
+            this.defaultItem = defaultItem;
+        }
+        
+        public void AddToPool(T ItemToAdd)
+        {
+            if (ItemToAdd is GameObject t)
+                t.SetActive(false);
+            pool.Enqueue(ItemToAdd);
+        }
+        
+        public T GetFromPool()
+        {
+            return (pool.Count == 0) ? pool.Dequeue() : Object.Instantiate(defaultItem);
+        }
+        
+        public IEnumerator AddToPoolAfterTime(T ItemToAdd, float time)
+        {
+            yield return new WaitForSeconds(time);
+            AddToPool(ItemToAdd);
+        }
     }
 }
